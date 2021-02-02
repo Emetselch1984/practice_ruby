51ee87b91635f852cfb10752c5f5c118db0a437d
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-02 14:00:29 +0900

    Added scrolling function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,18 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-//= require libs/hero-slider
+//= require libs/scroll-polyfill
 //= require libs/scroll
 //= require libs/text-animation
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const cb = function (el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.animate-title', cb);
+});
--- app/assets/javascripts/practice.js
+++ app/assets/javascripts/practice.js
@@ -1,12 +1,44 @@
-const child = document.querySelector('.child');
-const cb = function (entries,observer){
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            console.log('inview')
-        } else {
-            console.log('out view')
-        }
-    });
-}
-const io = new IntersectionObserver(cb);
-io.observe(child);
+// window.onloadは複数書くと上書きされてしまう
+window.onload = function() {
+    const child = document.querySelector('.child');
+    const cb = function (entries,observer){
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('inview');
+                console.log('inview')
+            } else {
+                entry.target.classList.remove('inview');
+                console.log('out view')
+            }
+        });
+
+    }
+    const options = {
+        root: null,
+        rootMargin: "-300px 0px",
+        threshold: [0, 0.5, 1]
+    };
+
+    const io = new IntersectionObserver(cb,options);
+    io.observe(child);
+
+
+};
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('#btn');
+    const h1 = document.querySelector('h1');
+    function hello() {
+        alert('hello');
+        // this.style.color = 'red';
+    }
+    function changeColor() {
+        h1.style.color = 'red';
+    }
+    function changeBgColor() {
+        h1.style.backgroundColor = 'green';
+    }
+    btn.addEventListener('click', changeColor);
+    btn.addEventListener('click', changeBgColor);
+    btn.removeEventListener('click', changeBgColor);
+});
+
